e45a049b5d313eb75126a22e756082b6a97ed343
kmkim <kkgg7527@gmail.com>
2022-09-13 01:58:33 +0900

    Add modal component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
+      <Todo text="Task01" />
+      <Todo text="Task02" />
+      <Todo text="Task03" />
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,29 @@
-function Todo() {
+import { useState } from 'react';
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
+function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
+
+  function closeModalHandler() {
+    console.log('click');
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="card">
-      <h1>my tools</h1>
+      <h1>{props.text}</h1>
       <div className="actions">
-        <button className="btn">삭제</button>
+        <button className="btn" onClick={deleteHandler}>
+          삭제
+        </button>
       </div>
+      {modalIsOpen && <Modal onCancel={closeModalHandler} />}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>
   );
 }
