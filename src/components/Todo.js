20f9e5b95667049231fe928a4463a7080478e5f9
kmkim <kkgg7527@gmail.com>
2022-09-12 23:18:07 +0900

    first commit

---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+function Todo() {
+  return (
+    <div className="card">
+      <h1>my tools</h1>
+      <div className="actions">
+        <button className="btn">삭제</button>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
